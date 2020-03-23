88a8531473be59d073b3225c591a2d7fb74db083
nurzhanar <57892976+nurzhanar@users.noreply.github.com>
2020-03-23 15:24:55 +0600

    Add files via upload

---
+++ homepagejava.js
@@ -0,0 +1,253 @@
+  function choosebutton11(){
+    var choose1 = document.getElementById("choose11");
+    var choose2 = document.getElementById("choose12");
+    var choose3 = document.getElementById("choose13");
+    var choose4 = document.getElementById("choose14");
+    var choose5 = document.getElementById("choose15");
+   
+    
+    choose1.style.display = "block";
+    choose2.style.display = "block";
+    choose3.style.display = "block";
+    choose4.style.display = "block";
+    choose5.style.display = "block";
+    
+  }
+  function choosebutton12(){
+    var choose1 = document.getElementById("choose11");
+    var choose2 = document.getElementById("choose12");
+    var choose3= document.getElementById("choose13");
+    var choose4 = document.getElementById("choose14");
+    var choose5 = document.getElementById("choose15");
+    choose1.style.display = "none";
+    choose2.style.display = "none";
+    choose3.style.display = "none";
+    choose4.style.display = "none";
+    choose5.style.display = "none";
+
+  }
+  function choosebutton21(){
+    var choose = document.getElementById("choose21");
+    var choose1 = document.getElementById("choose22");
+    var choose2 = document.getElementById("choose23");
+    var choose3 = document.getElementById("choose24");
+    choose.style.display = "block";
+    choose1.style.display = "block";
+    choose2.style.display = "block";
+    choose3.style.display = "block";
+
+  }
+  function choosebutton22(){
+    var choose = document.getElementById("choose21");
+    var choose1 = document.getElementById("choose22");
+    var choose2 = document.getElementById("choose23");
+    var choose3 = document.getElementById("choose24");
+    choose.style.display = "none";
+    choose1.style.display = "none";
+    choose2.style.display = "none";
+    choose3.style.display = "none";
+
+  }
+  function choosebutton31(){
+    var choose = document.getElementById("choose31");
+    var choose1 = document.getElementById("choose32");
+    var choose2 = document.getElementById("choose33");
+    var choose3 = document.getElementById("choose34");
+    choose.style.display = "block";
+    choose1.style.display = "block";
+    choose2.style.display = "block";
+    choose3.style.display = "block";
+
+  }
+  function choosebutton32(){
+    var choose = document.getElementById("choose31");
+    var choose1 = document.getElementById("choose32");
+    var choose2 = document.getElementById("choose33");
+    var choose3 = document.getElementById("choose34");
+    choose.style.display = "none";
+    choose1.style.display = "none";
+    choose2.style.display = "none";
+    choose3.style.display = "none";
+
+  }
+  function choosebutton41(){
+    var choose = document.getElementById("choose41");
+    var choose1 = document.getElementById("choose42");
+    var choose2 = document.getElementById("choose43");
+    var choose3 = document.getElementById("choose44");
+    choose.style.display = "block";
+    choose1.style.display = "block";
+    choose2.style.display = "block";
+    choose3.style.display = "block";
+
+  }
+  function choosebutton42(){
+    var choose = document.getElementById("choose41");
+    var choose1 = document.getElementById("choose42");
+    var choose2 = document.getElementById("choose43");
+    var choose3 = document.getElementById("choose44");
+    choose.style.display = "none";
+    choose1.style.display = "none";
+    choose2.style.display = "none";
+    choose3.style.display = "none";
+  }
+  function choosebutton51(){
+    var choose = document.getElementById("choose51");
+    var choose1 = document.getElementById("choose52");
+    var choose2 = document.getElementById("choose53");
+    var choose3 = document.getElementById("choose54");
+    choose.style.display = "block";
+    choose1.style.display = "block";
+    choose2.style.display = "block";
+    choose3.style.display = "block";
+
+  }
+  function choosebutton52(){
+    var choose = document.getElementById("choose51");
+    var choose1 = document.getElementById("choose52");
+    var choose2 = document.getElementById("choose53");
+    var choose3 = document.getElementById("choose54");
+    choose.style.display = "none";
+    choose1.style.display = "none";
+    choose2.style.display = "none";
+    choose3.style.display = "none";
+  }
+  function choosebutton61(){
+    var choose = document.getElementById("choose61");
+    var choose1 = document.getElementById("choose62");
+    var choose2 = document.getElementById("choose63");
+    var choose3 = document.getElementById("choose64");
+    choose.style.display = "block";
+    choose1.style.display = "block";
+    choose2.style.display = "block";
+    choose3.style.display = "block";
+
+  }
+  function choosebutton62(){
+    var choose = document.getElementById("choose61");
+    var choose1 = document.getElementById("choose62");
+    var choose2 = document.getElementById("choose63");
+    var choose3 = document.getElementById("choose64");
+    choose.style.display = "none";
+    choose1.style.display = "none";
+    choose2.style.display = "none";
+    choose3.style.display = "none";
+
+  }
+  function cleansernext(){
+    document.getElementById("cleansers").style.display="none";
+    document.getElementById("cleansers2").style.display="block";
+  }
+  function cleanserprev(){
+    document.getElementById("cleansers").style.display="block";
+    document.getElementById("cleansers2").style.display="none";
+  }
+  function byconcernnext(){
+    document.getElementById("byconcern").style.display="none";
+    document.getElementById("byconcern2").style.display="block";
+  }
+  function byconsernprev(){
+    document.getElementById("byconcern").style.display="block";
+    document.getElementById("byconcern2").style.display="none";
+  }
+  function moisturizernext(){
+    document.getElementById("moisturizers").style.display="none";
+    document.getElementById("moisturizers2").style.display="block";
+  }
+  function moisturizerprev(){
+    document.getElementById("moisturizers").style.display="block";
+    document.getElementById("moisturizers2").style.display="none";
+  }
+  function image1(){
+    document.getElementById("bgimage1").style.display="block";
+    document.getElementById("bgimage2").style.display="none";
+    document.getElementById("bgimage3").style.display="none";
+    document.getElementById("bgimage4").style.display="none";
+    document.getElementById("bgimage5").style.display="none";
+    document.getElementById("image1").style.border="1px solid black";
+    document.getElementById("image1").style.opacity="1";
+  }
+  function image2(){
+    document.getElementById("bgimage1").style.display="none";
+    document.getElementById("bgimage2").style.display="block";
+    document.getElementById("bgimage3").style.display="none";
+    document.getElementById("bgimage4").style.display="none";
+    document.getElementById("bgimage5").style.display="none";
+    document.getElementById("image2").style.border="1px solid black";
+    document.getElementById("image2").style.opacity="1";
+  }
+  function image3(){
+    document.getElementById("bgimage1").style.display="none";
+    document.getElementById("bgimage2").style.display="none";
+    document.getElementById("bgimage3").style.display="block";
+    document.getElementById("bgimage4").style.display="none";
+    document.getElementById("bgimage5").style.display="none";
+    document.getElementById("image3").style.border="1px solid black";
+    document.getElementById("image3").style.opacity="1";
+  }
+  function image4(){
+    document.getElementById("bgimage1").style.display="none";
+    document.getElementById("bgimage2").style.display="none";
+    document.getElementById("bgimage3").style.display="none";
+    document.getElementById("bgimage4").style.display="block";
+    document.getElementById("bgimage5").style.display="none";
+    document.getElementById("image4").style.border="1px solid black";
+    document.getElementById("image4").style.opacity="1";
+  }
+  function image5(){
+    document.getElementById("bgimage1").style.display="none";
+    document.getElementById("bgimage2").style.display="none";
+    document.getElementById("bgimage3").style.display="none";
+    document.getElementById("bgimage4").style.display="none";
+    document.getElementById("bgimage5").style.display="block";
+    document.getElementById("image5").style.border="1px solid black";
+    document.getElementById("image5").style.opacity="1";
+  }
+  function like1(){
+    document.getElementById("like11").style.display="none";
+    document.getElementById("like12").style.display="inline-block";
+  }
+  function dislike1(){
+    document.getElementById("like11").style.display="inline-block";
+    document.getElementById("like12").style.display="none";
+  }
+  function like2(){
+    document.getElementById("like21").style.display="none";
+    document.getElementById("like22").style.display="inline-block";
+  }
+  function dislike2(){
+    document.getElementById("like21").style.display="inline-block";
+    document.getElementById("like22").style.display="none";
+  }
+  function like3(){
+    document.getElementById("like31").style.display="none";
+    document.getElementById("like32").style.display="inline-block";
+  }
+  function dislike3(){
+    document.getElementById("like31").style.display="inline-block";
+    document.getElementById("like32").style.display="none";
+  }
+  function like4(){
+    document.getElementById("like41").style.display="none";
+    document.getElementById("like42").style.display="inline-block";
+  }
+  function dislike4(){
+    document.getElementById("like41").style.display="inline-block";
+    document.getElementById("like42").style.display="none";
+  }
+  function like5(){
+    document.getElementById("like51").style.display="none";
+    document.getElementById("like52").style.display="inline-block";
+  }
+  function dislike5(){
+    document.getElementById("like51").style.display="inline-block";
+    document.getElementById("like52").style.display="none";
+  }
+  function like6(){
+    document.getElementById("like61").style.display="none";
+    document.getElementById("like62").style.display="inline-block";
+  }
+  function dislike6(){
+    document.getElementById("like61").style.display="inline-block";
+    document.getElementById("like62").style.display="none";
+  }
